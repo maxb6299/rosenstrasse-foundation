864f506ad14aba8166521911f45e2d21038f2b6f
Max <126419161+maxb6299@users.noreply.github.com>
2023-07-13 10:17:40 -0400

    Make testimony route

--- server/routes/testimonyDatabase.js
+++ server/routes/testimonyDatabase.js
@@ -1,8 +1,34 @@
+const databaseController = require("../controllers/database.js");
+
 const express = require("express");
+const multer = require("multer");
+const upload = multer({ dest: "./database/testimonies/images" });
 const router = express.Router();
 
-router.get("/", async (req, res) => {
-  res.send("this is the testimony database");
+const FILE_PATH = "./database/team-members/data.json";
+
+router.get("/", (req, res) => {
+  databaseController.get_all_data(req, res, FILE_PATH);
+});
+
+router.post("/", (req, res) => {
+  databaseController.update_all_data(req, res, FILE_PATH);
+});
+
+router.get("/:id/image", (req, res) => {
+  databaseController.get_image(req, res, FILE_PATH);
+});
+
+router.post("/appendItem", (req, res) => {
+  databaseController.append_item(req, res, FILE_PATH);
+});
+
+router.post("/:id/image", upload.single("image"), (req, res) => {
+  databaseController.update_image(req, res, FILE_PATH);
+});
+
+router.delete("/:id", (req, res) => {
+  databaseController.delete_item(req, res, FILE_PATH);
 });
 
 module.exports = router;
