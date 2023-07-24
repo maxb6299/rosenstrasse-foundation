7343b0aa1e8b4e437c3f2e1e015f9b297c7a5cde
Max <126419161+maxb6299@users.noreply.github.com>
2023-07-24 18:50:01 -0400

    Transfer old site to new router structure

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/meet-the-team">Meet The Team</router-link>
   </nav>
   <router-view/>
 </template>
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,18 +1,6 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    Welcome to the Rosenstrasse Foundation website <br>
+    (under construction)
   </div>
 </template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
-</script>
