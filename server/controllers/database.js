43b5ff9b482c84250979d3259799b5a2144937c1
Max <126419161+maxb6299@users.noreply.github.com>
2023-07-24 10:55:22 -0400

    Seperate images to a different collection Write delete_item function

--- server/controllers/database.js
+++ server/controllers/database.js
@@ -39,10 +39,13 @@ exports.update_all_data = async (req, res, collection) => {
 
 exports.get_image = async (req, res, collection) => {
   try {
+    const imageCollection = collection + "Images";
     const id = req.params.id;
 
     const database = req.app.get("database");
-    const userData = await database.collection(collection).findOne({ _id: id });
+    const userData = await database
+      .collection(imageCollection)
+      .findOne({ _id: id });
 
     if (userData && userData.image) {
       const imageData = userData.image;
@@ -52,26 +55,27 @@ exports.get_image = async (req, res, collection) => {
       fs.writeFileSync(imagePath, imageBuffer);
 
       res.status(200).sendFile(imagePath);
-      console.log(`Successfully got photo from ${collection}`);
+      console.log(`Successfully got photo from ${imageCollection}`);
 
       clearTempDirectory();
     } else {
       res.status(404).json({
-        error: `Error getting image from collection ${collection}`,
+        error: `Error getting image from collection ${imageCollection}`,
       });
 
       console.log("Image not found for GET request");
     }
   } catch (error) {
-    res
-      .status(500)
-      .json({ error: `Error getting photo from collection ${collection}` });
-    console.log(`Error getting photo from ${collection}:`, error);
+    res.status(500).json({
+      error: `Error getting photo from collection ${imageCollection}`,
+    });
+    console.log(`Error getting photo from ${imageCollection}:`, error);
   }
 };
 
 exports.update_image = async (req, res, collection) => {
   try {
+    const imageCollection = collection + "Images";
     const id = req.params.id;
     const image = req.file;
     if (!image) {
@@ -85,86 +89,78 @@ exports.update_image = async (req, res, collection) => {
 
     const database = req.app.get("database");
     await database
-      .collection(collection)
+      .collection(imageCollection)
       .updateOne({ _id: id }, { $set: { image: imageData } }, { upsert: true });
 
     fs.unlinkSync(imagePath);
 
     res
       .status(200)
-      .json({ message: `Successfully updated photo from ${collection}` });
-    console.log(`Successfully updated photo from ${collection}`);
+      .json({ message: `Successfully updated photo from ${imageCollection}` });
+    console.log(`Successfully updated photo from ${imageCollection}`);
+
+    clearTempDirectory();
+  } catch (error) {
+    res.status(500).json({
+      error: `Error updating photo from collection ${imageCollection}`,
+    });
+    console.log(`Error updating photo from ${imageCollection}:`, error);
+  }
+};
+
+exports.append_item = async (req, res, collection) => {
+  try {
+    const id = req.params.id;
+    const data = req.body.data;
+
+    const database = req.app.get("database");
+    await database
+      .collection(collection)
+      .updateOne({ _id: id }, { $set: { data: data } }, { upsert: true });
+
+    res
+      .status(200)
+      .json({ message: `Successfully appended item in ${collection}` });
+    console.log(`Successfully appended item in ${collection}`);
 
     clearTempDirectory();
   } catch (error) {
     res
       .status(500)
-      .json({ error: `Error updating photo from collection ${collection}` });
-    console.log(`Error updating photo from ${collection}:`, error);
+      .json({ error: `Error appending item in collection ${collection}` });
+    console.log(`Error appending item in ${collection}:`, error);
   }
 };
 
-exports.append_item = async (req, res, filePath) => {
-  try {
-    const data = await readFile(filePath, "utf8");
-
-    let parsedData;
-    try {
-      parsedData = JSON.parse(data);
-    } catch (error) {
-      console.error(
-        "Error parsing data for POST request for appending an item",
-        error
-      );
-      res.status(500).send();
-      return;
-    }
-
-    parsedData.push(req.body);
-    const updatedData = JSON.stringify(parsedData);
-
-    await writeFile(filePath, updatedData, "utf8");
-    res.status(200).send();
-    console.log("Object appended in POST request");
-  } catch (error) {
-    res.status(500).send();
-    console.error("Error sending POST request appending an item", error);
-  }
-};
-
-exports.delete_item = async (req, res, filePath) => {
+exports.delete_item = async (req, res, collection) => {
   try {
     const id = req.params.id;
+    const database = req.app.get("database");
 
-    const data = await readFile(filePath, "utf8");
-    const parsedData = JSON.parse(data);
-
-    const item = parsedData.find((item) => item.id == id);
-    const itemIndex = parsedData.findIndex((item) => item.id == id);
-
-    if (itemIndex != -1 && itemIndex < parsedData.length) {
-      if (item.imagePath) {
-        try {
-          const absoluteImagePath = path.resolve(item.imagePath);
-          await unlink(absoluteImagePath);
-          console.log("Image sucessfully deleted");
-        } catch (error) {
-          console.error("Error deleting file", error);
-        }
-      }
-
-      parsedData.splice(itemIndex, 1); // deletes item
-      const updatedData = JSON.stringify(parsedData, null, 2);
-      await writeFile(filePath, updatedData, "utf8");
-      res.status(200).send();
-      console.log("Item sucessfully deleted");
-    } else {
-      res.status(404).send();
-      console.log("Item not found for DELETE request");
+    const imageCollection = collection + "Images";
+    try {
+      await database.collection(imageCollection).deleteOne({ _id: id });
+      console.log(`Successfully deleted image from ${imageCollection}`);
+    } catch (error) {
+      console.log(
+        `Error deleting image for user ${id} from ${imageCollection}:`,
+        error
+      );
     }
+
+    try {
+      await database.collection(collection).deleteOne({ _id: id });
+      console.log(`Successfully deleted user from ${collection}`);
+    } catch (error) {
+      console.log(`Error deleting user ${id} from ${collection}:`, error);
+      throw error;
+    }
+
+    res.status(200).json({ message: "success" });
   } catch (error) {
-    res.status(500).send();
-    console.log("Error reading file for DELETE request", error);
+    res.status(500).json({
+      error: `Error deleting user from collection ${collection}`,
+    });
   }
 };
 
