0475ded7bd5ea22a57f9f3dc0f34e74decae7e50
Max <126419161+maxb6299@users.noreply.github.com>
2023-07-25 17:36:11 -0400

    Write site footer

--- client/src/components/SiteFooter.vue
+++ client/src/components/SiteFooter.vue
@@ -1,3 +1,19 @@
 <template>
-  <div>Footer Placeholder</div>
+  <div class="content">
+    <div>© 2022 by the Rosenstraße Foundation | All rights reserved</div>
+    <a
+      href="https://www.paypal.com/donate/?business=PKN77PRKQMVDE&no_recurring=0&item_name=This+money+is+being+donated+to+the+Rosenstrasse+Foundation&currency_code=USD"
+      target="_blank"
+    >
+      <img src="@/assets/donate-button.png" alt="Donate" style="height: 38px" />
+    </a>
+  </div>
 </template>
+
+<style>
+.content {
+  display: flex;
+  align-items: center;
+  gap: 32px;
+}
+</style>
