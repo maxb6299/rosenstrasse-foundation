5251fb55d77c7e3f100928ea89b7647a43514d3a
Max <126419161+maxb6299@users.noreply.github.com>
2023-07-25 09:51:17 -0400

    Style body title

--- client/src/components/BodyTemplate.vue
+++ client/src/components/BodyTemplate.vue
@@ -1,7 +1,7 @@
 <template>
-    <div>
-        <div>{{title}}</div>
-        <div class="body"><slot></slot></div>
+    <div class="body-items">
+        <div class="body-title">{{title}}</div>
+        <div class="page-body"><slot></slot></div>
     </div>
 </template>
 
@@ -17,8 +17,35 @@ body {
     justify-content: center;
 }
 
-.body {
+.body-items {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    align-self: stretch;
+    padding: 10px 0px;
+    gap: 10px;
+}
+
+.page-body {
     width: 860px;
     background-color: #D9D9D9;
+}
+
+.body-title {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 315px;
+    padding: 10px 16px 16px 16px;
+    background: #9DBFD2;
+
+    color: #000;
+    font-family: Crimson Pro;
+    font-size: 24px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: normal;
+    text-decoration-line: underline;
 }
 </style>
