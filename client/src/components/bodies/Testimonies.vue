3dacac96173d1a48e6933a8a32e8f6fefd81fc3d
Max <126419161+maxb6299@users.noreply.github.com>
2023-07-24 22:10:35 -0400

    Write functions for testimony body

--- client/src/components/bodies/Testimonies.vue
+++ client/src/components/bodies/Testimonies.vue
@@ -3,3 +3,57 @@
         Testimonies
     </div>
 </template>
+
+<script>
+import databaseHelper from "@/_helpers/database.js"
+
+export default {
+    data() {
+        return {
+            data: {},
+            newTestimony: {
+                _id: '',
+                name: '',
+                description: '',
+                author: '',
+                categories: {
+                    rosenstrasseProtest: false,
+                    germanIntermarriage: false,
+                    womensResistance: false,
+                    germanCivilCourage: false
+                },
+                galleryIds: []
+            },
+            isAdmin: true,
+            databaseName: "testimonies"
+        }
+    },
+
+    methods: {
+        async getData() {
+            this.data = await databaseHelper.getData(this.databaseName);
+        },
+        getImageUrl(id) {
+            return databaseHelper.getImageUrl(this.databaseName, id);
+        },
+        async saveNewItem() {
+            await databaseHelper.saveNewItem(this.databaseName, this.newTestimony);
+            await this.getData();
+        },
+        async saveNewImage(id) {
+            await databaseHelper.saveImageToGallery(this.databaseName, id);
+        },
+        async deleteItem(id) {
+            await databaseHelper.deleteItem(this.databaseName, id);
+            await this.getData();
+        },
+        async deleteImage(id) {
+            await databaseHelper.deleteImage
+        }
+    },
+
+    beforeMount() {
+        this.getData()
+    }
+}
+</script>
