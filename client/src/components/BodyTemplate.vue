0f56b3501fbeb6b2d358e0d430812122f869529d
Max <126419161+maxb6299@users.noreply.github.com>
2023-07-24 21:23:07 -0400

    Move MeetTheTeam to a body template

---
+++ client/src/components/BodyTemplate.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <div class="body"><slot></slot></div>
+    </div>
+</template>
+
+<style>
+body {
+    display: flex;
+    justify-content: center;
+}
+
+.body {
+    width: 860px;
+    background-color: #D9D9D9;
+}
+</style>
