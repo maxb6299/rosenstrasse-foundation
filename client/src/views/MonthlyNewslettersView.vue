047b19bb6d34126ce4c8e59a9c09d575a00dc3c8
Max <126419161+maxb6299@users.noreply.github.com>
2023-07-26 17:09:59 -0400

    Fix typo

---
+++ client/src/views/MonthlyNewslettersView.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <BodyTemplate title="Monthly Newsletters">
+            <MonthlyNewslettersBody></MonthlyNewslettersBody>
+        </BodyTemplate>
+    </div>
+</template>
+
+<script>
+import BodyTemplate from "@/components/BodyTemplate.vue";
+import MonthlyNewslettersBody from "@/components/bodies/MonthlyNewslettersBody.vue";
+
+export default {
+    components: {
+        BodyTemplate,
+        MonthlyNewslettersBody,
+    },
+};
+</script>
