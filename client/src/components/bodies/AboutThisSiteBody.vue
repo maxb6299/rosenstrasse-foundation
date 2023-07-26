fb995414bcda9d626b30ce3d314c0c142f800a3c
Max <126419161+maxb6299@users.noreply.github.com>
2023-07-26 18:09:24 -0400

    Make about page

--- client/src/components/bodies/AboutThisSiteBody.vue
+++ client/src/components/bodies/AboutThisSiteBody.vue
@@ -1,3 +1,36 @@
 <template>
-    
+    <div class="big-group">
+        <div class="big-group" style="flex-direction: row;">
+            <div>
+                <img src="@/../public/assets/about.png" style="width: 450px;height: 303px;">
+                <div style="font-size: 16px;font-style: italic;">Frieda Thimm Rosenau with Heidi’s father, Werner, born June 28, 1929</div>
+            </div>
+            <div class="big-group">
+                        <h1>Mission Statement:</h1>
+                        <p>The mission of the Rosenstrasse Foundation is to commemorate, encourage, and educate about civil courage--concrete acts in opposition to injustice and human rights violations that defend the values of a pluralistic society.</p>
+            </div>
+        </div>
+
+        <div class="big-group">
+            
+
+            <div class="big-group">
+                <h1>
+                    Name and Initial Focus:
+                </h1>
+                <p>The Foundation takes its name from the Rosenstrasse Protest in central Berlin in early 1943, in which non-Jewish women married to Jewish men defied Hitler's regime to protest the capture of their husbands, leading to the men's release. The foundation is dedicated to the development of knowledge about this and other acts of women-led defiance in addition to acts of civil courage more generally, and to honoring the memory of the protesters, and creating a platform for the descendants of those involved in the protest to get to meet each other.</p>
+                <p>Central to the Foundation is the idea that everyday civilians can cause meaningful, democratic changes in the face of injustice. We honor examples of moral courage in spite of great personal risk, allowing these examples to remain in human memory and consciousness. Much of what is written and remembered about the Holocaust magnifies Hitler and other voices of evil. The Rosenstrasse Foundation strives instead to elevate the voices and actions of resistors and rescuers, giving them the last word, and shares the core goal of the Institute of Politics of civic engagement.</p>
+                <p>We espouse Vaclav Havel's principle of 'attempting to live in the truth,' available to all persons at all times regardless of whether they hold formal positions of influence. Twenty-first century dictatorships are often based on masses of persons conforming to the dictator's will, sometimes enthusiastically lending support. Our aim is to educate for actions in everyday life so that the defense of democracy happens all along rather than only after tyranny is established and taking the form of clandestine and momentous acts of violence. </p>
+            </div>
+
+            <div class="big-group">
+                <h1> 75th & 80th Anniversaries: 2018 & 2023 Events and Publications:</h1>
+                <p>February 27 to March 6 is the 80th anniversary of the historic Rosenstrasse Protest in the heart of Nazi Berlin. Non-Jewish women married to Jewish men who wore the star identifying them as Jews faced immediate deportation. The wives defied the regime with a weeklong protest, and won the release of their family members. This protest was the emblematic act of their resistance which began when Hitler took power. As a result, almost all German Jews who survived—around 11,000-- other than those in hiding, survived in mixed marriages. The Gestapo's practice was to deport intermarried Jews when their partners divorced or died.</p>
+                <p>Demonstrating undaunted moral courage, these non-Jewish women defied social norms and Gestapo terror, choosing to entwine their fate with Jewish partners rather than with Hitler's Third Reich. They rescued Jews openly only by risking their lives, most obviously during their street protest as they faced repeated threats to “clear the streets or be shot.” Their protest was emblematic of their continuous, public defiance which began ten years earlier when Hitler took power. They showed how ordinary persons can become heroic by building up their capacity for defiance and risk, day by day. The Rosenstrasse Foundation commemorates them as an ongoing primary example of civil courage.</p>
+                <p>The Rosenstrasse Foundation was established in 2018. Professors Volker Berghahn of Columbia University, Mordecai Paldiel of Yeshiva University, and Nathan Stoltzfus of Florida State University convened a conference on the 75th anniversary of the Rosenstrasse protest. The conference resulted in a book Women Defying Hitler: Rescue and Resistance under the Nazis (London: Bloomsbury, 2021). An accompanied Book launch video for the title can be seen here.</p>
+                <p>A related conference and publication, co-sponsored by the Rintels Professorship for Holocaust studies and FSU's Center for the Advancement of Human Rights, is The Power of Populism and the People (Bloomsbury, 2021)</p>
+                <p>To commemorate the 80th anniversary of the historic Rosenstrasse Protest, the Rosenstrasse Foundation initiated a series of events in Berlin, Washington DC, New York, Tallahassee, and Gainesville.</p>
+            </div>
+        </div>
+    </div>
 </template>
