6ade796d7bf84c7e1b0ac0977d9c5a1191918c2b
Max <126419161+maxb6299@users.noreply.github.com>
2023-07-26 12:13:06 -0400

    Add list of working links to home

--- client/src/components/bodies/HomeBody.vue
+++ client/src/components/bodies/HomeBody.vue
@@ -2,5 +2,37 @@
   <div>
     Welcome to the Rosenstrasse Foundation website <br />
     (under construction)
+    <div>
+      <NavigationBarDropdown title="Currently Working Links" :items="links"></NavigationBarDropdown>
+    </div>
   </div>
 </template>
+
+
+<script>
+import NavigationBarDropdown from '../NavigationBarDropdown.vue';
+export default {
+  components: {
+    NavigationBarDropdown
+  },
+
+   data() {
+    return {
+      links: [
+        {
+          title: "Home",
+          link: "/"
+        },
+        {
+          title: "Meet The Team",
+          link: "/meet-the-team"
+        },
+        {
+          title: "Personal Testimonies",
+          link: "/personal-testimonies"
+        }
+      ]
+    }
+  }
+}
+</script>
