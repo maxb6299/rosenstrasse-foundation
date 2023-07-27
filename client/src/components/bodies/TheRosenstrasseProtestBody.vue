16b828c022a9248a4186d731b86e615fd58211dd
Max <126419161+maxb6299@users.noreply.github.com>
2023-07-27 09:05:54 -0400

    Write rosenstrasse protest body

--- client/src/components/bodies/TheRosenstrasseProtestBody.vue
+++ client/src/components/bodies/TheRosenstrasseProtestBody.vue
@@ -1,1 +1,19 @@
-<template></template>
+<template>
+    <div class="big-group">
+        <div class="big-group" style="flex-direction: row; align-items: center;">
+            <div>
+                <img src="@/../public/assets/rosenstrasse-protest.webp" style="width:250px">
+                <div style="font-size: 16px;font-style: italic;">A photo of the Jewish Federation on Rosenstrasse where the Jewish husbands were held. The protests took place in the street around this building.</div>
+            </div>
+            <div class="big-group">
+                <h1>The Rosenstrasse Protest</h1>
+                <p>Before dawn on Saturday February 27, 1943  the SS, the Gestapo and the Berlin police launched a massive arrest intended to remove every person wearing the Jewish Star from Berlin. Jewish persons were grabbed from their jobs and homes, and persons seen wearing the Star were chased down and thrown onto the Gestapo’s covered furniture trucks headed for the deportation collection centers.  More than 8,000 of the last Jews in the city had been granted a “temporary” deferral from the deportations because they were married to German non-Jewish spouses. This deferral could have ended at any moment but had held up until that point, even though these Jews were “full Jews” according to the 1935 Nuremberg Laws.  There was one exception:  intermarried Jews were “temporarily” deferred only as long as their non-Jewish partners did not abandon them, these Jews were usually deported at once if their partners divorced.</p>
+            </div>
+        </div>
+        <div class="big-group">
+            <p>Nine days earlier on February 18, 1943, the Gauleiter for Greater Berlin, Joseph Goebbels, confided to his diary that “The Jews in Berlin will now once and for all be pushed out. With the final deadline of February 28 they are supposed to be first brought to collection centers and deported, up to 2,000, batch-by-batch, day-by-day. I have set for myself a goal to make Berlin entirely free of Jews by the middle or end of March at the latest.” This coincided with Reichsführer SS and Chief of German PoliceHeinrich Himmler’s order of November 5, 1942 that all remaining Jews be removed from Reich Territory, and beginning February 27, the Gestapo across the Reich began the arrests code named Elimination of Jews from the Reich Territory Actions.</p>
+            <p>Out of some 10,000 Jewish Berliners arrested , around 2,000 were men married to non-Jewish women who were rounded up and separated from the other deportees in a collection center at Rosenstraße 2-4 in central Berlin. As they discovered where their husbands were being held the wives trickled in to show their husbands that they were not willing to let them go, and a sense of solidarity among them begin to grow. one wife testified to German prosecutors that as she arrived early the next morning, she heard a cry growing louder and louder as she approached:   “Give us our husbands back!” This was a chorus the women repeated over the course of the following week.</p>
+            <p>On March 6, calling the protests by German wives an “unlovely scene,” Goebbels ordered the release of intermarried Jews. This was the only mass public demonstration by Germans in the Third Reich against the deportation of  Jews, and it was initiated and led by women.</p>
+        </div>
+    </div>
+</template>
