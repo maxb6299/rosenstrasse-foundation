669678778de46a1c2d4e4a16d6bd430685ac6368
Max <126419161+maxb6299@users.noreply.github.com>
2023-07-27 11:10:22 -0400

    Install vue3 google login

--- client/src/main.js
+++ client/src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import vue3GoogleLogin from "vue3-google-login";
 
 createApp(App).use(router).mount("#app");
+app.use(vue3GoogleLogin, {
+  clientId: `477777850658-pj298j9fdvlmtk12nct9pr3qa6cvs4c5.apps.googleusercontent.com`,
+});
