7c179917bb75da7ca6829c45da6cc6acf17c95d4
Max <126419161+maxb6299@users.noreply.github.com>
2023-07-27 09:29:21 -0400

    Make documents page

--- client/src/components/bodies/DocumentsBody.vue
+++ client/src/components/bodies/DocumentsBody.vue
@@ -1,1 +1,54 @@
-<template></template>
+<template>
+    <div class="big-group">
+        <a class="blue-button" v-for="(item, itemKey) in documents" :href="item.link">
+            {{ item.title }}
+        </a>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            documents: [
+                {
+                    title: 'OSS Report: [April 1, 1943]',
+                    link: 'https://4dac91b8-5102-4943-bb53-b8e5c1e67f53.filesusr.com/ugd/4c4715_9e68ff5f2815443eb2c36be74c87ea79.pdf'
+                },
+                {
+                    title: 'Names and Messages of Protestors at Rosenstrasse from the Wiener Holocaust Library Collections',
+                    link: ''
+                },
+                {
+                    title: 'German and English Translation of Hitler’s order in 1938 to divide intermarried Jews into privileged and non-privileged',
+                    link: 'https://avalon.law.yale.edu/imt/069-ps.asp'
+                },
+                {
+                    title: `Historical Evidence and Plausible History: Interpreting the Berlin Gestapo's Attempted "Final Roundup" of Jews (also known as the "Factory Action")`,
+                    link: 'https://4dac91b8-5102-4943-bb53-b8e5c1e67f53.filesusr.com/ugd/4c4715_838cb1eec51943de8116d9ea8691c46a.pdf'
+                },
+                {
+                    title: 'Nazi Conspiracy and Aggression Volume III Document No. 069-PS',
+                    link: 'https://4dac91b8-5102-4943-bb53-b8e5c1e67f53.filesusr.com/ugd/4c4715_838cb1eec51943de8116d9ea8691c46a.pdf'
+                },
+                {
+                    title: 'Resistance to Rosenstrasse History by Antonia Leugers',
+                    link: 'https://theologie-geschichte.de/ojs2/index.php/tg/article/view/133/148'
+                },
+                {
+                    title: 'Resistance to Rosenstrasse History II by Antonia Leugers',
+                    link: 'https://theologie-geschichte.de/ojs2/index.php/tg/article/view/536/575'
+                },
+                {
+                    title: 'After 80 years: Why we commemorate the Rosenstrasse protest - Jerusalem Post Article By Nathan Stoltzfus and Mordecai Paldiel [March 6, 2023]',
+                    link: ''
+                },
+                {
+                    title: `Appendices for “Protest in Hitler's "national community": popular unrest and the Nazi response”`,
+                    link: 'https://4dac91b8-5102-4943-bb53-b8e5c1e67f53.filesusr.com/ugd/4c4715_3f8b126e1e6a4a69bc068a6f90e369f1.pdf'
+                }
+            ]
+        }
+    }
+}
+</script>
