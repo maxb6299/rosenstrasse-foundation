93c643e4aa86dd45862348e8d5790ae5755425d1
Max <126419161+maxb6299@users.noreply.github.com>
2023-07-27 11:05:22 -0400

    Make login boilerplate

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -85,6 +85,11 @@ const routes = [
     name: "scholarly-debate",
     component: () => import("@/views/ScholarlyDebateView.vue"),
   },
+  {
+    path: "/login",
+    name: "login",
+    component: () => import("@/views/LoginView.vue"),
+  },
 ];
 
 const router = createRouter({
---
+++ client/src/views/LoginView.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <BodyTemplate title="Login">
+            <div>
+                
+            </div>
+        </BodyTemplate>
+    </div>
+</template>
+
+<script>
+import BodyTemplate from "@/components/BodyTemplate.vue";
+
+export default {
+    components: {
+        BodyTemplate,
+    },
+};
+</script>
