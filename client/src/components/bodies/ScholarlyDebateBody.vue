7c502296e4fc58512b353782119ada0e8fbbfedf
Max <126419161+maxb6299@users.noreply.github.com>
2023-07-27 10:32:00 -0400

    Write scholarly debate body

--- client/src/components/bodies/ScholarlyDebateBody.vue
+++ client/src/components/bodies/ScholarlyDebateBody.vue
@@ -1,1 +1,33 @@
-<template></template>
+<template>
+    <div class="big-group">
+        <div class="big-group">
+            <div class="title">Scholarly Debate by Sheighlin Haggerty</div>
+            <p>Prior to the 1960’s, the Rosenstrasse Protest was an event almost forgotten by history - the open protesting, by women, directed at the Nazi regime does not fit the narrative framework that most people understand. The idea that dissenting women could change the actions of the Gestapo seems like an anomaly within the context of the regime. Due to the uniqueness of this historical event, historians have different interpretations and engage in scholarly debate centered around the effects that the protesting Aryan wives had on the fates of their Jewish loved ones.</p>
+            <p>Historians have varying opinions taken surrounding the Rosenstrasse Protest, which can find its beginning in the 1960’s and 1970’s, when a few well-respected historians and authors wrote about the protest. After analysis, they all quickly concluded that the women who demonstrated outside of Rosenstrasse influenced the Gestapo to release the Jewish men and Mischling who were held there, instead of deporting them. As many of Berlin’s surviving Jews were intermarried, these historians feel that intermarriage and the status of their partners saved them from the cruel fates other Jews experienced during the Holocaust.</p>
+            <p>However, there are a few historians who conclude that in the context of Nazi power, it was not possible that a protest would have influenced the Gestapo. Some historians feel that the deportation of intermarried Jews was never part of the Nazi’s plans, and that the arrests and detainment at Rosenstrasse 2-4 were for registering Jewish men and their Mischling children. These claims highlight that the women, therefore, had no effect on the Gestapo.</p>
+            <p>Ultimately, the question comes down to whether the surviving intermarried Jews owe their fate to the Rosenstrasse protestors or the Gestapo. Some important questions to consider when contemplating the Rosenstrasse Protest center around the intent of the Nazi Regime. Why would they free these Jewish men and Mischling if their goal was to totally rid Berlin or Jews? Why were the Aryan women protesters not punished for their actions, as assisting or protesting on behalf of Jews was forbidden?</p>
+            <p>Some sources to read for more information on the scholarly debate surrounding the topic:</p>
+            <a target="_blank" href="https://4dac91b8-5102-4943-bb53-b8e5c1e67f53.filesusr.com/ugd/4c4715_66869210532f420aa16b7ace997b8cff.pdf">Laub, Dori. In: Historical Reflections. Summer, 2013, Vol. 39 Issue 2, pp 40-56</a>
+            <a target="_blank" href="https://4dac91b8-5102-4943-bb53-b8e5c1e67f53.filesusr.com/ugd/4c4715_f45525ca4c3d40d59f36b2ef58c2c7d8.pdf">Dori Laub, “In the Search of the Forgotten Rescuer: A Psychoanalytic Perspective.” Unpublished.</a>
+        </div>
+        
+        <br>
+        
+        <div class="big-group">
+            <div class="title">Plausible Evidence: Exchange of Arguments by Wolf GruNer and Dr. Nathan Stoltzfus</div>
+            <p>Dr. Nathan Stoltzfus responds to Wolf Gruner’s article, "The Factory Action and the Events at the Rosenstrasse in Berlin: Facts and Fictions about 27 February 1943 Sixty Years Later," in his own article "Historical Evidence and Plausible History: Interpreting the Berlin Gestapo's Attempted "Final Roundup" of Jews (also known as the "Factory Action")", arguing that the protest by non-Jews for their Jewish family members acted as the most reasonable impetus for the Gestapo's release of intermarried Jews incarcerated at Berlin's Rosenstrasse.</p>
+            <div class="big-group" style="flex-direction: row;">
+                <div class="blue-button">
+                    <a target="_blank" href="https://4dac91b8-5102-4943-bb53-b8e5c1e67f53.filesusr.com/ugd/4c4715_ce4a9ef62df84d65a4c8ef8fdf1778e0.pdf">
+                        Read Wolf Gruner’s article, "The Factory Action and the Events at the Rosenstrasse in Berlin: Facts and Fictions about 27 February 1943 Sixty Years Later,"
+                    </a>
+                </div>
+                <div class="blue-button">
+                    <a target="_blank" href="https://4dac91b8-5102-4943-bb53-b8e5c1e67f53.filesusr.com/ugd/4c4715_3e2a67a533614d8fb762c7c84536eee4.pdf">
+                        Read Nathan Stoltzfus' article, "Historical Evidence and Plausible History: Interpreting the Berlin Gestapo's Attempted "Final Roundup" of Jews (also known as the "Factory Action")"
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
