00bf8fd4cd569157b703d3ba2f602ff7f4b35647
Max <126419161+maxb6299@users.noreply.github.com>
2023-07-27 09:10:30 -0400

    Write donate body

--- client/src/components/bodies/DonateBody.vue
+++ client/src/components/bodies/DonateBody.vue
@@ -1,1 +1,15 @@
-<template></template>
+<template>
+    <div class="big-group" style="align-items: center;">
+        <div class="title">Support Us</div>
+        <div>Donate now to help support the research and educational operations that characterize the Rosenstrasse Foundation as a historical non-profit organization, as well as the program and events that we host and pride ourselves on developing.</div>
+        <a
+          href="https://www.paypal.com/donate/?business=PKN77PRKQMVDE&no_recurring=0&item_name=This+money+is+being+donated+to+the+Rosenstrasse+Foundation&currency_code=USD"
+          target="_blank"
+        >
+          <img
+            src="@/../public/assets/donate-button.png"
+            alt="Donate"
+          />
+        </a>
+    </div>
+</template>
