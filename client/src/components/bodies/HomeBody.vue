2b6a8e14e15f4157c6f5d3e80dcc324634074fa8
Max <126419161+maxb6299@users.noreply.github.com>
2023-07-27 10:51:24 -0400

    Wrtie home body

--- client/src/components/bodies/HomeBody.vue
+++ client/src/components/bodies/HomeBody.vue
@@ -1,38 +1,16 @@
 <template>
-  <div>
-    Welcome to the Rosenstrasse Foundation website <br />
-    (under construction)
+  <div  class="big-group" style="align-items: center;">
     <div>
-      <NavigationBarDropdown title="Currently Working Links" :items="links"></NavigationBarDropdown>
+      Welcome! Please click the dropdowns at the top to learn more about our mission and what we do. Permission must be obtained for use of any resources, biographies, or media shared on our website in any form. If you have any questions please feel free to reach out to us!
     </div>
+    <a
+      href="https://www.paypal.com/donate/?business=PKN77PRKQMVDE&no_recurring=0&item_name=This+money+is+being+donated+to+the+Rosenstrasse+Foundation&currency_code=USD"
+      target="_blank"
+    >
+      <img
+        src="@/../public/assets/donate-button.png"
+        alt="Donate"
+      />
+    </a>
   </div>
 </template>
-
-
-<script>
-import NavigationBarDropdown from '../NavigationBarDropdown.vue';
-export default {
-  components: {
-    NavigationBarDropdown
-  },
-
-   data() {
-    return {
-      links: [
-        {
-          title: "Home",
-          link: "/"
-        },
-        {
-          title: "Meet The Team",
-          link: "/meet-the-team"
-        },
-        {
-          title: "Personal Testimonies",
-          link: "/personal-testimonies"
-        }
-      ]
-    }
-  }
-}
-</script>
