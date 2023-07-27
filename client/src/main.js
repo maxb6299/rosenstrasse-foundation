29fb8047cb87c5a7ca28545fe5405a5c18931593
Max <126419161+maxb6299@users.noreply.github.com>
2023-07-27 11:27:55 -0400

    Fix bad google login install

--- client/src/main.js
+++ client/src/main.js
@@ -3,7 +3,11 @@ import App from "./App.vue";
 import router from "./router";
 import vue3GoogleLogin from "vue3-google-login";
 
-createApp(App).use(router).mount("#app");
+const app = createApp(App);
+
+app.use(router);
 app.use(vue3GoogleLogin, {
   clientId: `477777850658-pj298j9fdvlmtk12nct9pr3qa6cvs4c5.apps.googleusercontent.com`,
 });
+
+app.mount("#app");
