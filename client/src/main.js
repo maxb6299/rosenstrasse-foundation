fd12c83400fe61ebf65e904bea4ba1a00e01a0b7
Max <126419161+maxb6299@users.noreply.github.com>
2023-07-28 10:55:06 -0400

    Install pinia

--- client/src/main.js
+++ client/src/main.js
@@ -1,11 +1,13 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import { createPinia } from "pinia";
 import vue3GoogleLogin from "vue3-google-login";
 
 const app = createApp(App);
 
 app.use(router);
+app.use(createPinia());
 app.use(vue3GoogleLogin, {
   clientId: `477777850658-pj298j9fdvlmtk12nct9pr3qa6cvs4c5.apps.googleusercontent.com`,
 });
