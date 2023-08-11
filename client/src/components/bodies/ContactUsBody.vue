3d4ead7a2211339d6c58870b2aa3061252f04c87
Max <126419161+maxb6299@users.noreply.github.com>
2023-08-11 11:21:46 -0400

    Write contact us body

--- client/src/components/bodies/ContactUsBody.vue
+++ client/src/components/bodies/ContactUsBody.vue
@@ -1,1 +1,36 @@
-<template></template>
+<template>
+    <div class="big-group" style="align-items: center;">
+        <div>
+            <div class=title>Email us at: </div>
+            <div class="blue-button" onclick="window.open('mailto:rosenstrassefoundation@gmail.com', '_blank')" style="max-width: fit-content; padding: 12px;">
+                rosenstrassefoundation@gmail.com
+            </div>
+        </div>
+
+        <div>
+            <div id="mc_embed_shell">
+            <link href="//cdn-images.mailchimp.com/embedcode/classic-061523.css" rel="stylesheet" type="text/css">
+            <div id="mc_embed_signup">
+                <form action="https://rosenstrassefoundation.us21.list-manage.com/subscribe/post?u=f5d62e14a62fcf148c7f764a4&amp;id=fc2376b0ba&amp;f_id=009e66e1f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank">
+                    <div id="mc_embed_signup_scroll"><h2>Subscribe to our newsletter:</h2>
+                        <div class="mc-field-group"><label for="mce-EMAIL">Email Address <span class="asterisk">*</span></label><input type="email" name="EMAIL" class="required email" id="mce-EMAIL" required="true" value=""></div>
+                    <div id="mce-responses" class="clear foot">
+                        <div class="response" id="mce-error-response" style="display: none;"></div>
+                        <div class="response" id="mce-success-response" style="display: none;"></div>
+                    </div>
+                <div aria-hidden="true" style="position: absolute; left: -5000px;">
+                    /* real people should not fill this in and expect good things - do not remove this or risk form bot signups */
+                    <input type="text" name="b_f5d62e14a62fcf148c7f764a4_fc2376b0ba" tabindex="-1" value="">
+                </div>
+                    <div class="optionalParent">
+                        <div class="clear foot">
+                            <input type="submit" name="subscribe" id="mc-embedded-subscribe" class="button" value="Subscribe">
+                        </div>
+                    </div>
+                </div>
+            </form>
+            </div>
+        </div>
+        </div>
+    </div>
+</template>
