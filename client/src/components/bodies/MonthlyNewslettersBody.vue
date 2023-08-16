3c94cf90a4f08b07e7d989173f623f8c4877c48e
Max <126419161+maxb6299@users.noreply.github.com>
2023-08-16 15:10:14 -0400

    Write newsletters body with local sample data

--- client/src/components/bodies/MonthlyNewslettersBody.vue
+++ client/src/components/bodies/MonthlyNewslettersBody.vue
@@ -1,1 +1,30 @@
-<template></template>
+<template>
+    <div class="big-group">
+        <div v-for="(newsItem, newsItemKey) in news" :key="newsItemKey">
+            <a class="blue-button" :href="newsItem.link" target="_blank">{{ newsItem.title }}</a>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            news: [
+                {
+                    title: "january",
+                    link: "https://www.google.com/"
+                },
+                {
+                    title: "febuary",
+                    link: "https://www.google.com/"
+                },
+                {
+                    title: "march",
+                    link: "https://www.google.com/"
+                },
+            ]
+        }
+    }
+}
+</script>
