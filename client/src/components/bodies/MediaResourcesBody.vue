b7ed2922c84dde3119b40139cc9c93cba9560523
Max <126419161+maxb6299@users.noreply.github.com>
2023-08-16 14:52:43 -0400

    Write media resources body

--- client/src/components/bodies/MediaResourcesBody.vue
+++ client/src/components/bodies/MediaResourcesBody.vue
@@ -1,1 +1,23 @@
-<template></template>
+<template>
+    <div class="big-group" style="display: flex; align-items: center;">
+        <div>
+            <div class="title">Lectures</div>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=PLhN7ydN2kL8lX8rpAQlQRf1hWHsSMQPbF" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        </div>
+
+        <div>
+            <div class="title">Book Launches</div>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=PLhN7ydN2kL8knOBD2NjGRdhwChccPZpFn" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        </div>
+
+        <div>
+            <div class="title">Interviews</div>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=PLhN7ydN2kL8l1NI5FpAyAtOk5GqZ2sDTE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        </div>
+
+        <div>
+            <div class="title">Conference Panels</div>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=PLhN7ydN2kL8kK8yV25IfRMb7cFteqpNtV" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        </div>
+    </div>
+</template>
